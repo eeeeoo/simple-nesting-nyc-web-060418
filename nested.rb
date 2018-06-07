f27b0f034157f77f53c5ce8fc21329a8325fd949
Jee Yoon Lee <jlee66@wellesley.edu>
2018-06-07 09:31:42 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -126,7 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		#  programmer_hash[:dennis_ritchie][:languages].to_s
+		#  programmer_hash[:dennis_ritchie][:languages]
 		 binding.pry
 		#  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 end
